282782b9e0b1a9beea4ea541cf5195c557e2214d
Joel Wallis Jucá <joelwallis@gmail.com>
2018-07-27 14:02:33 -0300

    Refactoring of unit tests: using loops to run similar tests on multiple HTTP paths and methods

--- test/index.test.js
+++ test/index.test.js
@@ -21,27 +21,49 @@ describe('express-healthz', () => {
     expect(() => express().use(healthz)).to.not.throw()
   })
 
-  it('responds a 200 OK to either GET or HEAD requests to /health', () => {
-    ;['GET', 'HEAD'].forEach((verb) => {
-      const next = sinon.fake()
+  it(`calls next() for any HTTP request to neither /health and /healthz`, () => {
+    ;['/', '/something', '/something-else'].forEach((path) => {
+      ;['DELETE', 'GET', 'HEAD', 'OPTIONS', 'POST', 'PUT'].forEach((method) => {
+        const next = sinon.fake()
+        const req = { path, method }
+        const res = { status: sinon.fake(() => ({ end: sinon.fake() })) }
 
-      const req = { path: '/health', method: verb }
-      const res = { status: sinon.fake(() => ({ end: sinon.fake })) }
+        healthz(req, res, next)
 
-      healthz(req, res, next)
-
-      expect(res.status.callCount).to.equal(1)
-      expect(res.status.getCall(0).args[0]).to.equal(200)
+        expect(res.status.callCount).to.equal(0)
+        expect(next.callCount).to.equal(1)
+      })
     })
   })
 
-  it('calls next() for request diferent than GET /health', () => {
-    const next = sinon.fake()
+  ;['/health', '/healthz'].forEach((path) => {
+    describe(path, () => {
+      ;['GET', 'HEAD'].forEach((method) => {
+        it(`responds 200 OK to ${method} ${path}`, () => {
+          const next = sinon.fake()
 
-    ;['DELETE', 'GET', 'HEAD', 'POST', 'PUT'].forEach((verb) => {
-      healthz({ method: verb, path: '/something-else' }, {}, next)
+          const req = { path, method }
+          const res = { status: sinon.fake(() => ({ end: sinon.fake() })) }
+
+          healthz(req, res, next)
+
+          expect(res.status.callCount).to.equal(1)
+          expect(res.status.getCall(0).args[0]).to.equal(200)
+        })
+      })
+
+      ;['DELETE', 'OPTIONS', 'POST', 'PUT'].forEach((method) => {
+        it(`calls next() for ${method} ${path}`, () => {
+          const next = sinon.fake()
+          const req = { path, method }
+          const res = { status: sinon.fake(() => ({ end: sinon.fake() })) }
+
+          healthz(req, res, next)
+
+          expect(res.status.callCount).to.equal(0)
+          expect(next.callCount).to.equal(1)
+        })
+      })
     })
-
-    expect(next.callCount).to.equal(5)
   })
 })
